78f477b9f245a038d08dff277c06340f77fd4f86
Tobia Tesan <tobia.tesan@gmail.com>
2014-12-11 17:30:19 +0100

    Add main stub

--- src/puzzlesolver/PuzzleSolver.java
+++ src/puzzlesolver/PuzzleSolver.java
@@ -1,12 +1,53 @@
 package puzzlesolver;
 
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Iterator;
+import java.util.List;
+
 public class PuzzleSolver {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Path srcPath = Paths.get(args[0]);	
+		try {
+			List<String[]> tokenList = PuzzleFileParser.parseFile(srcPath);
+			Iterator<String[]> it = tokenList.iterator();
+			BasicPieceSet ps = new BasicPieceSet();
+			while (it.hasNext()) {
+				String[] tokens = it.next();
+				ps.addPiece(new BasicPuzzlePiece(
+						tokens[0],
+						tokens[1],
+						tokens[2],
+						tokens[3],
+						tokens[4],
+						tokens[5]
+						));
+			}
+			ps.solve();
+			
+			String output = "";
+			for (Iterator<Iterator<PuzzlePiece>> rowIt = ps.iterator(); 
+				 rowIt.hasNext();){
+				for (Iterator<PuzzlePiece> colIt = rowIt.next(); 
+					 colIt.hasNext();){
+					output += colIt.next();
+				}
+				output += "\n";
+			}
+			
+			System.out.println(output);
+		} catch (MalformedFileException e) {
+			System.out.println("Malformed file!");
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (MissingPiecesException e) {
+			System.out.println("Pieces seem to be missing from input.");
+		} 
 
 	}
 
