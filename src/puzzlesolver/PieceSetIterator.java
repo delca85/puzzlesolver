080d987f8f922f12114b7d454fb6804593a9a9c9
Tobia Tesan <tobia.tesan@gmail.com>
2014-12-11 12:21:29 +0100

    Rewrite PieceSetIterator as a 2D iterator

--- src/puzzlesolver/PieceSetIterator.java
+++ src/puzzlesolver/PieceSetIterator.java
@@ -3,24 +3,55 @@ package puzzlesolver;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
-public class PieceSetIterator implements Iterator<PuzzlePiece> {
+public class PieceSetIterator implements Iterator<Iterator<PuzzlePiece>> {
+	class PieceSetRowIterator implements Iterator<PuzzlePiece>{
+		private PuzzlePiece col;
+		
+		public PieceSetRowIterator(PuzzlePiece p) {
+			col = p;
+		}
+		
+		public boolean hasNext() {
+			if (col == null) {
+				return false;
+			}
+			return true;
+		}
+		
+		public PuzzlePiece next() {
+			if (col != null) {
+				PuzzlePiece res = col;
+				col = col.getEast();
+				return res;
+			} else {
+				throw new NoSuchElementException();
+			}
+
+		}
+		
+		public void remove() {
+			throw new UnsupportedOperationException();
+		}
+	}
+	
 	private PuzzlePiece row;
-	private PuzzlePiece col;
+	
 	public PieceSetIterator(PieceSet ps) {
 		row = ps.getNWCorner();
-		col = ps.getNWCorner();
 	}	
+	
 	public boolean hasNext() {
-		if (col.getEast() == null && row.getSouth() == null) {
+		if (row == null) {
 			return false;
 		}
 		return true;
 	}
-	public PuzzlePiece next() {
-		if (col.getEast() != null) {
-			return col.getEast();
-		} else if (row.getSouth() != null) {
-			return row.getSouth();
+	
+	public Iterator<PuzzlePiece> next() {
+		if (row != null) {
+			PuzzlePiece res = row;
+			row = row.getSouth();
+			return new PieceSetRowIterator(res);
 		} else {
 			throw new NoSuchElementException();
 		}
