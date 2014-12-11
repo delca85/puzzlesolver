f92bb58b98eb7bf5e15694a8cdc915bf3ca748ac
Tobia Tesan <tobia.tesan@gmail.com>
2014-12-11 11:59:28 +0100

    Refactor solver

--- src/puzzlesolver/BasicPieceSet.java
+++ src/puzzlesolver/BasicPieceSet.java
@@ -5,63 +5,76 @@ import java.util.HashMap;
 public class BasicPieceSet implements PieceSet {
 	
 	PuzzlePiece NWCorner = null;
-	HashMap<String,PuzzlePiece> map = new HashMap<String,PuzzlePiece>();
+	HashMap<String,PuzzlePiece> pieceHashMap = new HashMap<String,PuzzlePiece>();
 	
-	private static void NWrecursiveSolve(PuzzlePiece p, HashMap<String, PuzzlePiece> map) throws MissingPiecesException {
-		System.out.println(p.getId());
-		if (p.getWest() == null && !p.isWRow()) {
-			PuzzlePiece westNeighbour;
-			try { 
-				westNeighbour = map.get(p.getWestId());
-			} catch (NullPointerException e) { // Piece by this ID was not found.
-				throw new MissingPiecesException();
+	static class NWRecursiveSolver {
+		
+		private static boolean isFullyLinked(PuzzlePiece p) {
+			if (p == null) {
+				throw new IllegalArgumentException();
 			}
-			if (westNeighbour == null) {
-				throw new MissingPiecesException();
+			if ((!p.isSRow() && p.getSouth() == null) ||
+				(!p.isNRow() && p.getNorth() == null) ||
+				(!p.isWRow() && p.getWest() == null) ||
+				(!p.isERow() && p.getEast() == null)) {
+				return false;
 			}
-			p.setWest(westNeighbour);
-			westNeighbour.setEast(p);
-			NWrecursiveSolve(p.getWest(), map);
+			return true;
 		}
-		if (p.getSouth() == null && !p.isSRow()) {
-			PuzzlePiece southNeighbour;
-			try { 
+
+		private static void NWrecursiveSolve(PuzzlePiece p, HashMap<String, PuzzlePiece> map) throws MissingPiecesException {
+			
+			if (p.getSouth() == null && !p.isSRow()) {
+				// Still not linked south and needs linking
+				PuzzlePiece southNeighbour;
 				southNeighbour = map.get(p.getSouthId());
-			} catch (NullPointerException e) { // Piece by this ID was not found.
-				throw new MissingPiecesException();
+				if (southNeighbour == null) {
+					// Piece by this ID was not found.
+					// Method got called /before/ loading all the pieces.
+					// (Depends on HashMap behaviour)
+					throw new MissingPiecesException();
+				}
+				p.setSouth(southNeighbour);
+				southNeighbour.setNorth(p);
+				// Link south neighbour with piece in both directions
+				if (!isFullyLinked(p.getSouth())) {
+					/*
+					 * This is not exactly necessary but 
+					 * avoids visiting a piece only to return
+					 * after finding out there was nothing to do
+					 */
+					NWrecursiveSolve(p.getSouth(), map);
+				}
 			}
-			if (southNeighbour == null) {
-				throw new MissingPiecesException();
+			
+			if (p.getEast() == null && !p.isERow()) {
+				PuzzlePiece eastNeighbour;
+				try { 
+					eastNeighbour = map.get(p.getEastId());
+				} catch (NullPointerException e) { 
+					throw new MissingPiecesException();
+				}
+				if (eastNeighbour == null) {
+					throw new MissingPiecesException();
+				}			
+				p.setEast(eastNeighbour);
+				eastNeighbour.setWest(p);
+				if (!isFullyLinked(p.getEast())) {
+					NWrecursiveSolve(p.getEast(), map);
+				}
 			}
-			p.setSouth(southNeighbour);
-			southNeighbour.setNorth(p);
-			NWrecursiveSolve(p.getSouth(), map);
-		}
-		if (p.getEast() == null && !p.isERow()) {
-			PuzzlePiece eastNeighbour;
-			try { 
-				eastNeighbour = map.get(p.getEastId());
-			} catch (NullPointerException e) { // Piece by this ID was not found.
-				throw new MissingPiecesException();
-			}
-			if (eastNeighbour == null) {
-				throw new MissingPiecesException();
-			}			
-			p.setEast(eastNeighbour);
-			eastNeighbour.setWest(p);
-			NWrecursiveSolve(p.getEast(), map);
-		}
-	};
+		};
+	}
 	
 	public void addPiece(PuzzlePiece p) {
 		if (p.isNWCorner()) {
 			NWCorner = p;
 		}
-		map.put(p.getId(), p);
+		pieceHashMap.put(p.getId(), p);
 	}
 
 	public void solve() throws MissingPiecesException {
-		NWrecursiveSolve(NWCorner, map);
+		NWRecursiveSolver.NWrecursiveSolve(NWCorner, pieceHashMap);
 	}
 
 	public PieceSetIterator iterator() {
