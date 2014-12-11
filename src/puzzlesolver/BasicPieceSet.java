725b69105da1d80b3c3269c0266c8a3769e900f5
Tobia Tesan <tobia.tesan@gmail.com>
2014-12-11 20:38:13 +0100

    Add BFS solver

--- src/puzzlesolver/BasicPieceSet.java
+++ src/puzzlesolver/BasicPieceSet.java
@@ -1,6 +1,10 @@
 package puzzlesolver;
 
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Set;
 
 public class BasicPieceSet implements PieceSet {
 	
@@ -22,6 +26,77 @@ public class BasicPieceSet implements PieceSet {
 			return true;
 		}
 
+		private static void BFSSolve (PuzzlePiece root, HashMap<String, PuzzlePiece> map) throws MissingPiecesException {
+			Queue<PuzzlePiece> queued = new LinkedList<PuzzlePiece>();
+			Set<PuzzlePiece> visited = new HashSet<PuzzlePiece>();
+			visited.add(root);
+			queued.add(root);
+			while (!queued.isEmpty()) {
+
+				PuzzlePiece t = queued.remove();
+				
+				if (t == null) {
+					return;
+				}
+
+				PuzzlePiece n = t.getNorth();
+				PuzzlePiece s = t.getSouth();
+				PuzzlePiece w = t.getWest();
+				PuzzlePiece e = t.getEast();
+					
+				if (!visited.contains(n)) {
+					if (!t.isNRow() && n == null) { 
+						PuzzlePiece northNeighbour = map.get(t.getNorthId());
+						if (northNeighbour == null) {
+							throw new MissingPiecesException();
+						}
+						t.setNorth(northNeighbour);
+					}
+					visited.add(n);
+					queued.add(n);
+				}
+
+				if (!visited.contains(s)) {
+					if (!t.isSRow() && s == null) { 
+						PuzzlePiece southNeighbour = map.get(t.getSouthId());
+						if (southNeighbour == null) {
+							throw new MissingPiecesException();
+						}
+						t.setSouth(southNeighbour);
+					}
+					visited.add(s);
+					queued.add(s);
+				}
+
+				if (!visited.contains(w)) {
+					if (!t.isWRow() && w == null) { 
+						PuzzlePiece westNeighbour = map.get(t.getWestId());
+						if (westNeighbour == null) {
+							throw new MissingPiecesException();
+						}						
+						t.setWest(westNeighbour);
+					}
+					visited.add(w);
+					queued.add(w);
+				}
+
+				if (!visited.contains(e)) {
+					if (!t.isERow() && e == null) { 
+						PuzzlePiece eastNeighbour = map.get(t.getEastId());
+						if (eastNeighbour == null) {
+							throw new MissingPiecesException();
+						}						
+						t.setEast(eastNeighbour);
+					}
+					visited.add(e);
+					queued.add(e);
+				}
+
+				
+			}
+			
+		}
+		
 		private static void NWrecursiveSolve(PuzzlePiece p, HashMap<String, PuzzlePiece> map) throws MissingPiecesException {
 			
 			if (p.getSouth() == null && !p.isSRow()) {
@@ -74,7 +149,7 @@ public class BasicPieceSet implements PieceSet {
 	}
 
 	public void solve() throws MissingPiecesException {
-		NWRecursiveSolver.NWrecursiveSolve(NWCorner, pieceHashMap);
+		NWRecursiveSolver.BFSSolve(NWCorner, pieceHashMap);
 	}
 
 	public PieceSetIterator iterator() {
