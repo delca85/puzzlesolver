6733ebd01c4554383fd70d80f7494b42264bd390
Tobia Tesan <tobia.tesan@gmail.com>
2014-12-13 11:09:35 +0100

    Catch malformed args in PUzzleSolver

--- src/puzzlesolver/PuzzleSolver.java
+++ src/puzzlesolver/PuzzleSolver.java
@@ -12,34 +12,38 @@ public class PuzzleSolver {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		Path srcPath = Paths.get(args[0]);
-		Path dstPath = Paths.get(args[1]);
 		try {
-			List<String[]> tokenList = PuzzleFileParser.parseFile(srcPath);
-			Iterator<String[]> it = tokenList.iterator();
-			
-			BasicPuzzle ps = new BasicPuzzle();
-			while (it.hasNext()) {
-				String[] tokens = it.next();
-				ps.addPiece(new BasicPuzzlePiece(
-						tokens[0],
-						tokens[1],
-						tokens[2],
-						tokens[4],
-						tokens[5],
-						tokens[3]
-						));
+			Path srcPath = Paths.get(args[0]);
+			Path dstPath = Paths.get(args[1]);
+			try {
+				List<String[]> tokenList = PuzzleFileParser.parseFile(srcPath);
+				Iterator<String[]> it = tokenList.iterator();
+				
+				BasicPuzzle ps = new BasicPuzzle();
+				while (it.hasNext()) {
+					String[] tokens = it.next();
+					ps.addPiece(new BasicPuzzlePiece(
+							tokens[0],
+							tokens[1],
+							tokens[2],
+							tokens[4],
+							tokens[5],
+							tokens[3]
+							));
+				}
+				
+				PuzzlePrinter p = new FilePuzzlePrinter(dstPath);
+				p.print(ps);
+			} catch (MalformedFileException e) {
+				System.out.println("Malformed file!");
+			} catch (IOException e) {
+				e.printStackTrace();
+			} catch (MissingPiecesException e) {
+				System.out.println("Pieces seem to be missing from input.");
 			}
-			
-			PuzzlePrinter p = new FilePuzzlePrinter(dstPath);
-			p.print(ps);
-		} catch (MalformedFileException e) {
-			System.out.println("Malformed file!");
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (MissingPiecesException e) {
-			System.out.println("Pieces seem to be missing from input.");
-		} 
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("Usage: java PuzzleSolver input.txt output.txt");
+		}
 
 	}
 
