6b0d4038dedeb332a456a3c60aac2bc52d5e2c88
Tobia Tesan <tobia.tesan@gmail.com>
2014-12-13 14:43:12 +0100

    Check for non existing file, etc

--- puzzlesolver/PuzzleSolver.java
+++ puzzlesolver/PuzzleSolver.java
@@ -3,46 +3,48 @@ package puzzlesolver;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.NoSuchFileException;
 import java.util.Iterator;
 import java.util.List;
 
 public class PuzzleSolver {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
 		try {
 			Path srcPath = Paths.get(args[0]);
 			Path dstPath = Paths.get(args[1]);
 			try {
 				List<String[]> tokenList = PuzzleFileParser.parseFile(srcPath);
-				Iterator<String[]> it = tokenList.iterator();
-				
-				BasicPuzzle ps = new BasicPuzzle();
-				while (it.hasNext()) {
-					String[] tokens = it.next();
-					ps.addPiece(new BasicPuzzlePiece(
-							tokens[0],
-							tokens[1],
-							tokens[2],
-							tokens[4],
-							tokens[5],
-							tokens[3]
-							));
+				try {
+					Iterator<String[]> it = tokenList.iterator();
+					
+					BasicPuzzle ps = new BasicPuzzle();
+					while (it.hasNext()) {
+						String[] tokens = it.next();
+						ps.addPiece(new BasicPuzzlePiece(
+								tokens[0],
+								tokens[1],
+								tokens[2],
+								tokens[4],
+								tokens[5],
+								tokens[3]
+								));
+					}
+					
+					PuzzlePrinter p = new FilePuzzlePrinter(dstPath);
+					p.print(ps);
+				} catch (MalformedFileException e) {
+					System.err.println("Malformed file!");
+				} catch (MissingPiecesException e) {
+					System.err.println("Pieces seem to be missing from input.");
 				}
-				
-				PuzzlePrinter p = new FilePuzzlePrinter(dstPath);
-				p.print(ps);
-			} catch (MalformedFileException e) {
-				System.out.println("Malformed file!");
+			} catch (NoSuchFileException e) {
+				System.err.println("File not found!");
 			} catch (IOException e) {
 				e.printStackTrace();
-			} catch (MissingPiecesException e) {
-				System.out.println("Pieces seem to be missing from input.");
-			}
+			} 
 		} catch (ArrayIndexOutOfBoundsException e) {
-			System.out.println("Usage: java PuzzleSolver input.txt output.txt");
+			System.err.println("Usage: java PuzzleSolver input.txt output.txt");
 		}
 
 	}
